dec825a186a9fc2051aa0bd308759600baed926a
Restyled.io <commits@restyled.io>
2022-02-19 16:58:47 +0000

    Restyled by clang-format

--- minishell.c
+++ minishell.c
@@ -12,32 +12,23 @@
 
 #include "minishell.h"
 
-static t_bool valid(int argc, char *argv[])
-{
-    (void)argc;
-    (void)argv;
-    return (TRUE);
+static t_bool valid(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+  return (TRUE);
 }
 
-void	free_app(t_app *self)
-{
-    (void)self;
-}
+void free_app(t_app *self) { (void)self; }
 
-int main(int argc, char *argv[])
-{
-    t_app self;
+int main(int argc, char *argv[]) {
+  t_app self;
 
-    // Load app setup
-    self = (t_app) {
-        0
-    };
-    if (valid(argc - 1, &argv[1]))
-    {
-        // Do stuff
-        free_app(&self);
-    }
-    else
-        ft_putendl("Error");
-    return (0);
+  // Load app setup
+  self = (t_app){0};
+  if (valid(argc - 1, &argv[1])) {
+    // Do stuff
+    free_app(&self);
+  } else
+    ft_putendl("Error");
+  return (0);
 }
