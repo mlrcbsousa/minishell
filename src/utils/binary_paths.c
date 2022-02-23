2d1e9b47a165ae1dcda276568893454ac39e8fba
Manuel Sousa <mlrcbsousa@gmail.com>
2022-02-23 22:09:05 +0100

    Suggestion for refactor of binary paths.

--- src/utils/binary_paths.c
+++ src/utils/binary_paths.c
@@ -3,30 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   binary_paths.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
+/*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 21:39:23 by ngregori          #+#    #+#             */
-/*   Updated: 2022/02/22 22:02:50 by ngregori         ###   ########.fr       */
+/*   Updated: 2022/02/23 22:07:34 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //Change naming to executables_paths maybe?
-char	**get_binary_paths(char **envp) {
-	int i = -1;
+char	**get_binary_paths(char **envp)
+{
 	char **paths;
 
 	paths = NULL;
-	while(envp[++i]) // loops over each env variable until it find path, then creates an array with each path to binary files
-	{
-		if(ft_strncmp("PATH=", envp[i], 5) == 0)
-		{
-			paths = ft_split(&envp[i][5], ':');
+	// loops over each env variable until it find path, then creates an array
+	// with each path to binary files
+	while(*envp && ft_strncmp("PATH=", *envp, 5))
+		envp++;
+	if (*envp)
+		paths = ft_split(*envp + 5, ':');
+	//need to add "/" to the end of each binary path next so that when we are
+	// trying to run the commands in a loop we don't need to add "/" to each command
 
-			//need to add "/" to the end of each binary path next so that when we are trying to run the commands in a loop we don't need to add "/" to each command
-		}
-	}
-	
 	return paths;
 }
