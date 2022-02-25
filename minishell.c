40c071bccc5c6825f5654f723d7d129eaad5e696
Manuel Sousa <mlrcbsousa@gmail.com>
2022-02-25 21:34:01 +0100

    Add readline library and use getenv function.

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:09:14 by msousa            #+#    #+#             */
-/*   Updated: 2022/02/25 21:10:25 by msousa           ###   ########.fr       */
+/*   Updated: 2022/02/25 21:33:32 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int main(void) // no need for arguments can use `getenv("PATH")`
 	t_stack tokens;
 
 	// ignore "Ctrl-C"
+	// will need to save this to untoggle on child
+	// will need to use something else other then signal ignore
 	signal(SIGINT, SIG_IGN);
 	// ignore "Ctrl-\"
   signal(SIGQUIT, SIG_IGN);
@@ -32,6 +34,8 @@ int main(void) // no need for arguments can use `getenv("PATH")`
 		// 1. stdin loop
 		line = readline("~$ ");
 		size = ft_strlen(line);
+
+		printf("%s\n", line);
 
 		// 2. handle Ctrl-D
 		// aparently no signal for this
