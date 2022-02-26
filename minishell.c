fd49492181701e2a6a75fe69ae7b66ef40a6cf2c
nuno <nuno_c11@hotmail.com>
2022-02-26 22:09:49 +0000

    working history & adjusted EOF signal message

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:09:14 by msousa            #+#    #+#             */
-/*   Updated: 2022/02/26 19:50:52 by ngregori         ###   ########.fr       */
+/*   Updated: 2022/02/26 22:09:18 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,10 @@ int main(void) // no need for arguments can use `getenv("PATH")`
 		// 1. stdin loop
 		line = readline("~$ ");
 		if(!line) {
-			ft_putchar('\n');
+			printf("exit\n");
 			exit(0);
 		}
+		add_history(line);
 		size = ft_strlen(line);
 
 		// 2. handle Ctrl-D
