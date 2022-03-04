90f8125b8d5144066ef3f8af85fd990ea33a21fa
nuno <nuno_c11@hotmail.com>
2022-03-04 21:52:16 +0000

    fixed printed message and exit status to match bash implementation

--- src/run.c
+++ src/run.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   run.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
+/*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 20:41:51 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/04 20:54:10 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/04 21:49:47 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	run(t_command *command, t_app *self)
 	pid_t pid;
 
 	pid = fork();
-  if(pid == 0)
+
+  	if(pid == 0)
 	{
 		// restore sigint in the child process
 		signal(SIGINT, self->sigint_handler);
@@ -31,15 +32,15 @@ void	run(t_command *command, t_app *self)
 		printf("%s\n", *command->argv);
 		if (execve(*command->argv, command->argv, self->env) == -1)
 		{
-      printf("minishell: command not found: %s\n", *command->argv);
-			exit(1);
-    }
-  }
+      		printf("%s: command not found\n", *command->argv);
+			exit(127);
+    	}
+  	}
 	else if (pid < 0) {
 		perror("fork");
 		return;
 	}
 
 	// wait for process to finish
-  while (waitpid(pid, NULL, 0) <= 0);
+  waitpid(pid, NULL, 0);
 }
