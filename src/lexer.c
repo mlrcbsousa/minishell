8c274fad0a9b6f0d0df60da7a6e919e227139d99
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 17:52:37 +0000

    Fix norminette for lexer.c.

--- src/lexer.c
+++ src/lexer.c
@@ -6,16 +6,12 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 18:52:15 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/08 17:44:05 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 17:52:23 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// if operator (pipe, redirect or semi-colon) create new token store operator
-//     and then create another token
-// if default create a new token once reach whitespace
-// otherwise manage state (is inside quotes or not)
 static void	lexer_state_default(t_lexer *lexer)
 {
 	if (lexer->type == LEXICAL_QUOTE)
@@ -35,18 +31,14 @@ static void	lexer_state_default(t_lexer *lexer)
 static void	lexer_state_dquote(t_lexer *lexer)
 {
 	lexer->token->data[lexer->data_i++] = lexer->c;
-	// if closing quote
 	if (lexer->type == LEXICAL_DQUOTE)
-		// exit quote grouping
 		lexer->state = STATE_DEFAULT;
 }
 
 static void	lexer_state_quote(t_lexer *lexer)
 {
 	lexer->token->data[lexer->data_i++] = lexer->c;
-	// if closing quote
 	if (lexer->type == LEXICAL_QUOTE)
-		// exit quote grouping
 		lexer->state = STATE_DEFAULT;
 }
 
@@ -62,7 +54,8 @@ static void	lexer_type_null(t_lexer *lexer)
 
 // while going through line check for various contextual grammar
 // first check by state (is inside quotes or not) then type (token type info)
-void lexical_analysis(char* line, int size, t_stack *analysed, t_env *env)
+// TODO: refactor loop body to function
+void	lexical_analysis(char *line, int size, t_stack *analysed, t_env *env)
 {
 	t_lexer	lexer;
 
@@ -71,39 +64,30 @@ void lexical_analysis(char* line, int size, t_stack *analysed, t_env *env)
 	analysed->token = (t_token *)malloc(sizeof(t_token));
 	lexer = (t_lexer){analysed->token, STATE_DEFAULT, size, line, 0, 0, 0, 0};
 	token_init(lexer.token, lexer.size);
-
 	while (line[lexer.line_i])
 	{
 		lexer.c = line[lexer.line_i];
 		lexer.type = lexical_type(lexer.c);
-
 		if (lexer.state == STATE_DEFAULT)
 			lexer_state_default(&lexer);
 		else if (lexer.state == STATE_DQUOTE)
 			lexer_state_dquote(&lexer);
 		else if (lexer.state == STATE_QUOTE)
 			lexer_state_quote(&lexer);
-
 		if (lexer.type == LEXICAL_NULL)
 			lexer_type_null(&lexer);
-
 		lexer.line_i++;
 	}
-
-	// TODO: refactor loop body to function
 	lexer.c = line[lexer.line_i];
 	lexer.type = lexical_type(lexer.c);
-
 	if (lexer.state == STATE_DEFAULT)
 		lexer_state_default(&lexer);
 	else if (lexer.state == STATE_DQUOTE)
 		lexer_state_dquote(&lexer);
 	else if (lexer.state == STATE_QUOTE)
 		lexer_state_quote(&lexer);
-
 	if (lexer.type == LEXICAL_NULL)
 		lexer_type_null(&lexer);
-
 	lexer_expand(analysed->token, env);
 	analysed->size = tokens_length(analysed->token);
 }
