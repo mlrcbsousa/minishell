de5e1a962fa64fdeb5ed4a63a8b96771a7ced7ee
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 18:03:08 +0000

    Fix norminette for parser.c.

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 18:37:24 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/06 16:20:34 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 18:02:55 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,17 +48,14 @@
 
 // Making this a struct for now because don't know if will need more
 // info to pass around for these functions
-int parse(t_stack *analysed, t_astree **astree)
+int	parse(t_stack *analysed, t_astree **astree)
 {
 	t_parser	parser;
 
 	if (!analysed->size)
 		return (-1);
-
 	parser.current_token = analysed->token;
 	*astree = command_line(&parser);
-
-	// If command filter didn't reach the end of the token list
 	if (parser.current_token && parser.current_token->type)
 	{
 		printf("Syntax Error at: %s\n", parser.current_token->data);
@@ -67,39 +64,31 @@ int parse(t_stack *analysed, t_astree **astree)
 	return (0);
 }
 
-// Utility function that checks the current token against the given type
-//			and moves the current token to the next on the list either way
-// If the types match it returns TRUE
-// If the types match and a buffer argument was supplied, current token data
-//			is also copied over
-// Otherwise returns FALSE
 t_bool	match(int token_type, char **buffer, t_parser *parser)
 {
 	t_token	*token;
 
 	token = parser->current_token;
-	if (token == NULL)
+	if (!token)
 		return (FALSE);
-
 	if (token->type == token_type)
 	{
 		if (buffer)
 		{
-			*buffer = (char*)malloc(ft_strlen(token->data) + 1);
+			*buffer = (char *)malloc(ft_strlen(token->data) + 1);
 			ft_strcpy(*buffer, token->data);
 		}
 		parser->current_token = token->next;
 		return (TRUE);
 	}
-
 	parser->current_token = token->next;
 	return (FALSE);
 }
 
 t_astree	*command_line(t_parser *parser)
 {
-	t_token *save;
-	t_astree *node;
+	t_token		*save;
+	t_astree	*node;
 
 	save = parser->current_token;
 	node = command_line_a(parser);
@@ -115,23 +104,18 @@ t_astree	*command_line(t_parser *parser)
 
 t_astree	*command_line_a(t_parser *parser)
 {
-	t_astree *command_node;
-	t_astree *command_line_node;
-	t_astree *result;
+	t_astree	*command_node;
+	t_astree	*command_line_node;
+	t_astree	*result;
 
-	// test first token if is a command
 	command_node = command(parser);
 	if (!command_node)
 		return (NULL);
-
-	// test next token if is a pipe operator
 	if (!match(LEXICAL_PIPE, NULL, parser))
 	{
 		astree_destroy(command_node);
 		return (NULL);
 	}
-
-	// test next token if is a command line
 	command_line_node = command_line(parser);
 	if (!command_node)
 	{
@@ -146,7 +130,7 @@ t_astree	*command_line_a(t_parser *parser)
 
 t_astree	*command(t_parser *parser)
 {
-	t_token	*save;
+	t_token		*save;
 	t_astree	*node;
 
 	save = parser->current_token;
