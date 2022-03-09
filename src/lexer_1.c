6f14c96cf339d7cde66d27a50f7f8d685197a75f
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 17:55:06 +0000

    Fix norminette for lexer_1.c.

--- src/lexer_1.c
+++ src/lexer_1.c
@@ -6,7 +6,7 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 18:52:15 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/06 16:16:37 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 17:54:54 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,14 @@ void	lexer_end_read_token(t_lexer *lexer)
 {
 	if (lexer->data_i > 0)
 	{
-		// end previous token data
 		lexer->token->data[lexer->data_i] = 0;
 		lexer->token->next = (t_token *)malloc(sizeof(t_token));
 		lexer->token = lexer->token->next;
-		// make new token data size smaller by how far along in line size
 		token_init(lexer->token, lexer->size - lexer->line_i);
-		// reset token data index
 		lexer->data_i = 0;
 	}
 }
 
-// if quotes or double quotes create grouping using state change
 void	lexer_type_quote(t_lexer *lexer)
 {
 	lexer->state = STATE_QUOTE;
@@ -35,12 +31,10 @@ void	lexer_type_quote(t_lexer *lexer)
 	lexer->token->type = LEXICAL_DEFAULT;
 }
 
-// If quote or double quote change state
 void	lexer_type_dquote(t_lexer *lexer)
 {
 	lexer->state = STATE_DQUOTE;
 	lexer->token->data[lexer->data_i++] = LEXICAL_DQUOTE;
-	// since not operator token use default type
 	lexer->token->type = LEXICAL_DEFAULT;
 }
 
@@ -52,12 +46,8 @@ void	lexer_type_default(t_lexer *lexer)
 
 void	lexer_type_operator(t_lexer *lexer)
 {
-	// if token data already has something split off and start new token
 	lexer_end_read_token(lexer);
-
 	lexer->token->data[0] = lexer->type;
-
-	// handle double redirects
 	if (lexer->type == LEXICAL_GREATER
 		&& lexer->line[lexer->line_i + 1] == LEXICAL_GREATER)
 	{
@@ -76,10 +66,7 @@ void	lexer_type_operator(t_lexer *lexer)
 	}
 	else
 		lexer->token->data[1] = 0;
-
 	lexer->token->type = lexer->type;
-
-	// when operator token need an additional token created
 	lexer->token->next = (t_token *)malloc(sizeof(t_token));
 	lexer->token = lexer->token->next;
 	token_init(lexer->token, lexer->size - lexer->line_i);
