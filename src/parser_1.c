b8754123945c29b9acf8aa84c752ceb556df0caf
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 17:57:35 +0000

    Fix norminette for parser_1.c.

--- src/parser_1.c
+++ src/parser_1.c
@@ -6,7 +6,7 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 22:56:04 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/08 16:51:18 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 17:57:26 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,23 @@ t_astree	*command_a(t_parser *parser)
 	t_astree	*simple_command_node;
 	t_astree	*redirect_node;
 	t_astree	*result;
-	char* filename;
+	char		*filename;
 
-	// test first token if is a simple command
 	simple_command_node = simple_command(parser);
 	if (!simple_command_node)
 		return (NULL);
-
-	// test next token if is a redirect operator
 	if (!match(LEXICAL_LESSER, NULL, parser))
 	{
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
-	// test next token if is a default and save filename
 	if (!match(LEXICAL_DEFAULT, &filename, parser))
 	{
 		free(filename);
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
 	redirect_node = redirect_command(parser);
-
-	// Create node return it
 	result = (t_astree *)malloc(sizeof(t_astree));
 	astree_set_type(result, NODE_REDIRECT_IN);
 	astree_set_data(result, filename);
@@ -54,31 +46,23 @@ t_astree	*command_b(t_parser *parser)
 	t_astree	*simple_command_node;
 	t_astree	*redirect_node;
 	t_astree	*result;
-	char* filename;
+	char		*filename;
 
-	// test first token if is a simple command
 	simple_command_node = simple_command(parser);
 	if (!simple_command_node)
 		return (NULL);
-
-	// test next token if is a redirect operator
 	if (!match(LEXICAL_GREATER, NULL, parser))
 	{
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
-	// test next token if is a default and save filename
 	if (!match(LEXICAL_DEFAULT, &filename, parser))
 	{
 		free(filename);
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
 	redirect_node = redirect_command(parser);
-
-	// Create node return it
 	result = (t_astree *)malloc(sizeof(t_astree));
 	astree_set_type(result, NODE_REDIRECT_OUT);
 	astree_set_data(result, filename);
@@ -91,31 +75,23 @@ t_astree	*command_c(t_parser *parser)
 	t_astree	*simple_command_node;
 	t_astree	*redirect_node;
 	t_astree	*result;
-	char* delimiter;
+	char		*delimiter;
 
-	// test first token if is a simple command
 	simple_command_node = simple_command(parser);
 	if (!simple_command_node)
 		return (NULL);
-
-	// test next token if is a redirect operator
 	if (!match(LEXICAL_LLESSER, NULL, parser))
 	{
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
-	// test next token if is a default and save delimiter
 	if (!match(LEXICAL_DEFAULT, &delimiter, parser))
 	{
 		free(delimiter);
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
 	redirect_node = redirect_command(parser);
-
-	// Create node return it
 	result = (t_astree *)malloc(sizeof(t_astree));
 	astree_set_type(result, NODE_HEREDOC);
 	astree_set_data(result, delimiter);
@@ -128,31 +104,23 @@ t_astree	*command_d(t_parser *parser)
 	t_astree	*simple_command_node;
 	t_astree	*redirect_node;
 	t_astree	*result;
-	char* filename;
+	char		*filename;
 
-	// test first token if is a simple command
 	simple_command_node = simple_command(parser);
 	if (!simple_command_node)
 		return (NULL);
-
-	// test next token if is a redirect operator
 	if (!match(LEXICAL_GGREATER, NULL, parser))
 	{
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
-	// test next token if is a default and save filename
 	if (!match(LEXICAL_DEFAULT, &filename, parser))
 	{
 		free(filename);
 		astree_destroy(simple_command_node);
 		return (NULL);
 	}
-
 	redirect_node = redirect_command(parser);
-
-	// Create node return it
 	result = (t_astree *)malloc(sizeof(t_astree));
 	astree_set_type(result, NODE_APPEND);
 	astree_set_data(result, filename);
@@ -164,12 +132,11 @@ t_astree	*simple_command(t_parser *parser)
 {
 	t_astree	*token_list_node;
 	t_astree	*result;
-	char	*command_path;
+	char		*command_path;
 
 	if (!match(LEXICAL_DEFAULT, &command_path, parser))
-			return (NULL);
+		return (NULL);
 	token_list_node = token_list(parser);
-	// we don't check whether token_list_node is NULL since its allowed to be
 	result = (t_astree *)malloc(sizeof(t_astree));
 	astree_set_type(result, NODE_CMDPATH);
 	astree_set_data(result, command_path);
