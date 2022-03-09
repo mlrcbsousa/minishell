6d1e215c458d1cce82846087cc2d36d30d33ccb6
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 18:18:11 +0000

    Fix norminette for minishell.c.

--- minishell.c
+++ minishell.c
@@ -6,76 +6,55 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:09:14 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/08 17:42:09 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 18:18:05 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int argc, char *argv[], char *env[])
+// TODO: make internal exit free all memory
+// TODO: make 'exit' show in same line as prompt
+// TEST
+// printf("successful parsing\n");
+// print_astree(astree);
+// printf("========================================= NODES END\n");
+int	main(int argc, char *argv[], char *env[])
 {
-	t_app self;
-	char *line;
-	size_t size;
-	t_stack analysed;
-	t_astree *astree;
+	t_app		self;
+	char		*line;
+	size_t		size;
+	t_stack		analysed;
+	t_astree	*astree;
 
 	(void)argc;
 	(void)argv;
 	g_return = 0;
-
-	// overide "Ctrl-C"
-	self = (t_app){ NULL, NULL, signal(SIGINT, sigint_handler), NULL };
-
-	// ignore "Ctrl-\"
+	self = (t_app){NULL, NULL, signal(SIGINT, sigint_handler), NULL};
 	signal(SIGQUIT, SIG_IGN);
-
 	set_env(&self, env);
-
 	while (1)
 	{
 		line = NULL;
 		size = 0;
 		analysed = (t_stack){NULL, 0};
-
-		// 1. get standard input
 		line = readline("~$ ");
-		if(!line) {
-			// TODO: make internal exit free all memory
-			// TODO: make 'exit' show in same line as prompt
+		if (!line)
+		{
 			env_destroy(&self);
 			printf("exit\n");
 			exit(0);
 		}
 		add_history(line);
 		size = ft_strlen(line);
-
-		// 2. break up line into tokens
 		lexical_analysis(line, size, &analysed, self.env);
 		free(line);
-
-		// TEST - analysed tokens
 		print_tokens(analysed.token);
-
-		// 3. parse stack of tokens into an abstract syntax tree
 		astree = NULL;
 		if (!analysed.size || parse(&analysed, &astree))
-			// continue ;
-			;
-		else
-		{
-			printf("successful parsing\n");
-			print_astree(astree);
-			printf("========================================= NODES END\n");
-		}
-		// TODO: check if needed
+			continue ;
 		self.astree = astree;
 		token_destroy(analysed.token);
-
-		// 5. execute syntax tree
 		execute_tree(astree, &self);
-
-		// 6. free memory
 		astree_destroy(astree);
 	}
 	return (0);
