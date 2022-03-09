27f3090047b22969ddeb4c83d9663de2b0b3e024
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 18:12:34 +0000

    Fix norminette for token.c.

--- src/token.c
+++ src/token.c
@@ -6,36 +6,36 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 19:42:47 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/06 15:59:00 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 18:12:28 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int lexical_type(char c)
+int	lexical_type(char c)
 {
 	if (c == LEXICAL_QUOTE
-			|| c == LEXICAL_PIPE
-			|| c == LEXICAL_QUOTE
-			|| c == LEXICAL_DQUOTE
-			|| c == LEXICAL_WHITESPACE
-			|| c == LEXICAL_GREATER
-			|| c == LEXICAL_LESSER
-			|| c == LEXICAL_NULL)
+		|| c == LEXICAL_PIPE
+		|| c == LEXICAL_QUOTE
+		|| c == LEXICAL_DQUOTE
+		|| c == LEXICAL_WHITESPACE
+		|| c == LEXICAL_GREATER
+		|| c == LEXICAL_LESSER
+		|| c == LEXICAL_NULL)
 		return (c);
 	else
 		return (LEXICAL_DEFAULT);
 }
 
-void token_init(t_token *token, int size)
+void	token_init(t_token *token, int size)
 {
 	token->data = (char *)malloc(size + 1);
-	*token->data = 0; // consider ft_bzero
+	*token->data = 0;
 	token->type = LEXICAL_NULL;
 	token->next = NULL;
 }
 
-void token_destroy(t_token *token)
+void	token_destroy(t_token *token)
 {
 	if (!token)
 		return ;
@@ -44,9 +44,9 @@ void token_destroy(t_token *token)
 	free(token);
 }
 
-int tokens_length(t_token *token)
+int	tokens_length(t_token *token)
 {
-	int length;
+	int	length;
 
 	length = 0;
 	while (token)
