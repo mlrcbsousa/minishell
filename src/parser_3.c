d8c4b29d46a91096e0f9a9f16ff68b0ca01ad64c
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 18:00:37 +0000

    Fix norminette for parser_3.c.

--- src/parser_3.c
+++ src/parser_3.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser_2.c                                         :+:      :+:    :+:   */
+/*   parser_3.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 16:21:16 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/06 16:22:08 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 18:00:32 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 t_astree	*token_list(t_parser *parser)
 {
-	t_token *save;
-	t_astree *node;
+	t_token		*save;
+	t_astree	*node;
 
 	save = parser->current_token;
 	node = token_list_a(parser);
@@ -29,12 +29,11 @@ t_astree	*token_list_a(t_parser *parser)
 {
 	t_astree	*token_list_node;
 	t_astree	*result;
-	char* arg;
+	char		*arg;
 
 	if (!match(LEXICAL_DEFAULT, &arg, parser))
-			return (NULL);
+		return (NULL);
 	token_list_node = token_list(parser);
-	// we don't check whether token_list_node is NULL since its allowed to be
 	result = (t_astree *)malloc(sizeof(t_astree));
 	astree_set_type(result, NODE_ARGUMENT);
 	astree_set_data(result, arg);
