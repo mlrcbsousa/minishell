501695e759b15088527f17fdf6c83884b66ebe5d
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 18:09:55 +0000

    Fix norminette for run.c.

--- src/run.c
+++ src/run.c
@@ -6,7 +6,7 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 20:41:51 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/09 17:44:54 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 18:09:45 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,54 +38,40 @@ void	run_setup_io_out(t_io *io)
 
 void	run_setup_io(t_command *command, t_env *env)
 {
-	// redirect stdin from file if specified
-	if(command->redirect_in)
+	if (command->redirect_in)
 		run_setup_io_in(command->redirect_in, env);
-
-  // redirect stdout to file if specified
-	if(command->redirect_out)
+	if (command->redirect_out)
 		run_setup_io_out(command->redirect_out);
-
-	// read from stdin pipe
 	if (command->stdin_pipe)
 		dup2(command->pipe_read, STDIN_FILENO);
-
-	// write to stdout pipe
 	if (command->stdout_pipe)
 		dup2(command->pipe_write, STDOUT_FILENO);
 }
 
+// printf("%s\n", *command->argv);
 void	run(t_command *command, t_app *self)
 {
-	pid_t pid;
-	int stdout_fd;
+	pid_t	pid;
+	int		stdout_fd;
 
 	pid = fork();
-  if(pid == 0)
+	if (pid == 0)
 	{
-		// restore sigint in the child process
 		signal(SIGINT, self->sigint_handler);
-
-		// handle pipes and redirects
-		stdout_fd = dup(STDOUT_FILENO); // to restore at the end
+		stdout_fd = dup(STDOUT_FILENO);
 		run_setup_io(command, self->env);
-
-		// check if file is in bin path
 		find_binary_path(command, self->env);
-		printf("%s\n", *command->argv);
-
 		if (execve(*command->argv, command->argv, get_env_raw(self)) == -1)
 		{
-			dup2(stdout_fd, STDOUT_FILENO); // restore stdout
-      printf("%s: command not found\n", *command->argv);
+			dup2(stdout_fd, STDOUT_FILENO);
+			printf("%s: command not found\n", *command->argv);
 			exit(127);
 		}
 	}
-	else if (pid < 0) {
+	else if (pid < 0)
+	{
 		perror("fork");
-		return;
+		return ;
 	}
-
-	// wait for process to finish
-  waitpid(pid, NULL, 0);
+	waitpid(pid, NULL, 0);
 }
