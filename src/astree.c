9c240a93122500768afbe2f3c1d9be0b8a16946a
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 15:15:04 +0000

    Fix norminette for astree.c.

--- src/astree.c
+++ src/astree.c
@@ -3,43 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   astree.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
+/*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 21:07:59 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/05 15:22:06 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 15:14:42 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void astree_add_branches(t_astree *root , t_astree *left , t_astree *right)
+void	astree_add_branches(t_astree *root, t_astree *left, t_astree *right)
 {
-	if(!root)
+	if (!root)
 		return ;
 	root->left = left;
 	root->right = right;
 }
 
-void astree_set_type(t_astree *node , t_node type)
+void	astree_set_type(t_astree *node, t_node type)
 {
-	if(!node)
+	if (!node)
 		return ;
 	node->type = type;
 }
 
-void astree_set_data(t_astree *node , char *data)
+void	astree_set_data(t_astree *node, char *data)
 {
-	if(!node || !data)
+	if (!node || !data)
 		return ;
 	node->data = data;
 	if (!node->type)
 		node->type = NODE_DATA;
 }
 
-void astree_destroy(t_astree *node)
+void	astree_destroy(t_astree *node)
 {
 	if (!node)
-		return;
+		return ;
 	if (node->data)
 		free(node->data);
 	astree_destroy(node->left);
