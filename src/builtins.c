078a11e38437091bb6a703c4059f6b89924109e2
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-09 17:14:21 +0000

    Fix norminette for builtins.c.

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 20:18:59 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/08 18:40:05 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/09 17:13:25 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 t_builtin	*get_builtin(char *cmd_path)
 {
 	static t_builtin_def	builtins[7] = {
-		{"echo", builtin_echo},
-		{"cd", builtin_cd},
-		{"pwd", builtin_pwd},
-		{"export", builtin_export},
-		{"unset", builtin_unset},
-		{"env", builtin_env},
-		{"exit", builtin_exit},
+	{"echo", builtin_echo},
+	{"cd", builtin_cd},
+	{"pwd", builtin_pwd},
+	{"export", builtin_export},
+	{"unset", builtin_unset},
+	{"env", builtin_env},
+	{"exit", builtin_exit},
 	};
-	int i;
+	int						i;
 
 	i = 0;
 	while (i < 7)
@@ -38,10 +38,10 @@ t_builtin	*get_builtin(char *cmd_path)
 int	builtin_echo(t_command *command, t_app *self)
 {
 	t_bool	with_n;
-	int	i;
-	int stdout_fd;
+	int		i;
+	int		stdout_fd;
 
-	stdout_fd = dup(STDOUT_FILENO); // to restore at the end
+	stdout_fd = dup(STDOUT_FILENO);
 	run_setup_io(command, self->env);
 	with_n = ft_streq(command->argv[1], "-n");
 	i = 1;
@@ -52,63 +52,55 @@ int	builtin_echo(t_command *command, t_app *self)
 	printf("%s", command->argv[i]);
 	if (!ft_streq(command->argv[1], "-n"))
 		printf("\n");
-	dup2(stdout_fd, STDOUT_FILENO); // restore stdout
+	dup2(stdout_fd, STDOUT_FILENO);
 	return (0);
 }
 
 int	builtin_cd(t_command *command, t_app *self)
 {
-	int stdout_fd;
+	int		stdout_fd;
+	char	*path;
 
-	stdout_fd = dup(STDOUT_FILENO); // to restore at the end
+	stdout_fd = dup(STDOUT_FILENO);
 	run_setup_io(command, self->env);
-
-	if (command->argc == 1) {
-		char *home = get_env("HOME", self->env);
-
-		if(home)
+	if (command->argc == 1)
+	{
+		path = get_env("HOME", self->env);
+		if (!path)
 		{
-			if(chdir(home) == -1) {
-				ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-				ft_putstr_fd(home, STDERR_FILENO);
-				ft_putstr_fd(": ", STDERR_FILENO);
-				perror("");
-				return (1);
-			}
-			return (0);
+			ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
+			return (1);
 		}
-
-		ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
-		return 1;
 	}
-
-	if(chdir(command->argv[1]) == -1) {
+	else
+		path = command->argv[1];
+	if (chdir(path) == -1)
+	{
 		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-		ft_putstr_fd(command->argv[1], STDERR_FILENO);
+		ft_putstr_fd(path, STDERR_FILENO);
 		ft_putstr_fd(": ", STDERR_FILENO);
 		perror("");
+		return (1);
 	}
-
-	dup2(stdout_fd, STDOUT_FILENO); // restore stdout
+	dup2(stdout_fd, STDOUT_FILENO);
 	return (0);
 }
 
 int	builtin_export(t_command *command, t_app *self)
 {
-	int i;
-	char **splitted;
-	t_env *new_env;
-	t_env *temp;
+	int		i;
+	char	**splitted;
+	t_env	*new_env;
+	t_env	*temp;
 	char	*found;
-	int stdout_fd;
+	int		stdout_fd;
 
-	stdout_fd = dup(STDOUT_FILENO); // to restore at the end
+	stdout_fd = dup(STDOUT_FILENO);
 	run_setup_io(command, self->env);
-
 	i = 1;
-	while(command->argv[i]) {
+	while (command->argv[i])
+	{
 		temp = self->env;
-
 		splitted = ft_split_single(command->argv[i], '=');
 		if (!splitted)
 		{
@@ -121,23 +113,20 @@ int	builtin_export(t_command *command, t_app *self)
 			i++;
 			continue ;
 		}
-
 		found = get_env(splitted[0], self->env);
-
-		if(!is_valid_identifier(splitted[0]))
+		if (!is_valid_identifier(splitted[0]))
 		{
 			ft_putstr_fd("minishell: export: `", STDOUT_FILENO);
 			ft_putstr_fd(splitted[0], STDOUT_FILENO);
 			ft_putstr_fd("': not a valid identifier\n", STDOUT_FILENO);
 		}
-
 		if (found)
 		{
-			while(temp)
+			while (temp)
 			{
-				if(ft_streq(temp->key, splitted[0]))
+				if (ft_streq(temp->key, splitted[0]))
 				{
-					if(!ft_streq(temp->value, splitted[1]))
+					if (!ft_streq(temp->value, splitted[1]))
 					{
 						free(temp->value);
 						free(found);
@@ -160,48 +149,42 @@ int	builtin_export(t_command *command, t_app *self)
 		ft_free_string_arrays(splitted);
 		i++;
 	}
-
-	dup2(stdout_fd, STDOUT_FILENO); // restore stdout
+	dup2(stdout_fd, STDOUT_FILENO);
 	return (0);
 }
 
 int	builtin_unset(t_command *command, t_app *self)
 {
-	int i;
-	t_env *temp;
-	t_env *previous;
-	int stdout_fd;
+	int		i;
+	t_env	*temp;
+	t_env	*previous;
+	int		stdout_fd;
 
-	stdout_fd = dup(STDOUT_FILENO); // to restore at the end
+	stdout_fd = dup(STDOUT_FILENO);
 	run_setup_io(command, self->env);
-
 	i = 1;
-	while(command->argv[i]) {
+	while (command->argv[i])
+	{
 		temp = self->env;
 		previous = self->env;
-
-		while(temp) {
-			if(strcmp(temp->key, command->argv[i]) == 0) {
+		while (temp)
+		{
+			if (strcmp(temp->key, command->argv[i]) == 0)
+			{
 				free(temp->key);
-				temp->key = NULL;
-
 				free(temp->value);
-				temp->value=NULL;
-
-				if(temp == previous) {
+				if (temp == previous)
 					self->env = temp->next;
-				}
 				else
 					previous->next = temp->next;
 				free(temp);
-				break;
+				break ;
 			}
 			previous = temp;
 			temp = temp->next;
 		}
 		i++;
 	}
-
-	dup2(stdout_fd, STDOUT_FILENO); // restore stdout
+	dup2(stdout_fd, STDOUT_FILENO);
 	return (0);
 }
