e81991e05e5de0d9e26e19ec646e3ad581ec9428
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-11 23:20:43 +0000

    Fix small bug in echo with extra space.

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 20:18:59 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/10 02:48:29 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/11 23:18:38 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,7 @@ int	builtin_echo(t_command *command, t_app *self)
 		i = 2;
 	while (i < command->argc - 1)
 		printf("%s ", command->argv[i++]);
-	if (command->argv[i])
-		printf("%s", command->argv[i]);
+	printf("%s", command->argv[i]);
 	if (!with_n)
 		printf("\n");
 	dup2(stdout_fd, STDOUT_FILENO);
